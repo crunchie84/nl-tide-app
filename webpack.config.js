b356148bc13772067a2fc13666b891ab7598cf05
Mark van Straten <mvanstraten@schubergphilis.com>
2020-06-30 14:19:07 +0200

    Do not minify development deploys

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,25 @@ const path = require('path');
 const slsw = require('serverless-webpack');
 
 module.exports = {
-  mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
+  mode:
+    slsw.lib.webpack.isLocal || slsw.lib.options.stage === '' || slsw.lib.options.stage === 'dev'
+      ? 'development'
+      : 'production',
+  optimization: {
+    // do not minimize when deploying to development or running locally
+    minimize:
+      slsw.lib.webpack.isLocal || slsw.lib.options.stage === '' || slsw.lib.options.stage === 'dev' ? false : true,
+  },
   entry: slsw.lib.entries,
   devtool: 'source-map',
+  //externals: [{'aws-sdk': 'commonjs aws-sdk'}],
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
     symlinks: false,
     cacheWithContext: false,
     alias: {
       '@lib': path.resolve(__dirname, './src/lib'),
-      // '@foo': path.resolve(__dirname, './src/foo'),
+      '@api': path.resolve(__dirname, './src/api'),
     },
   },
   output: {
@@ -25,7 +34,10 @@ module.exports = {
   module: {
     rules: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
-      { test: /\.tsx?$/, loader: 'ts-loader' },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
     ],
   },
 };
